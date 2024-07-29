e114e4395d9d82fab7dd2a10d554b9c1e18aa8cd
IrinaAndriakoto <119145568+IrinaAndriakoto@users.noreply.github.com>
2024-07-29 15:43:26 +0300

    deploy

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'postgres.railway.internal',
 	'username' => 'postgres',
-	'password' => 'irina',
-	'database' => 'madaimmo',
+	'password' => 'yXTfydSRkqzYtExskqqfIatLrAbSEKjz',
+	'database' => 'railway',
 	'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
